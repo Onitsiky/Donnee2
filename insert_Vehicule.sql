cd26bd3206d9e7e013d3f6129fb28ead450e7853
Onitsiky <hei.onitsiky@gmail.com>
2022-06-05 07:00:29 +0300

    feat : adding new datas to insert_vehicule file

--- insert_Vehicule.sql
+++ insert_Vehicule.sql
@@ -1,1 +1,10 @@
-insert into Vehicules(nombre_places, places_dispo,matricule) values (20,3,'3215 TAV'), (20,3, '1234 MUW'),(20,1, '3412 TAR'),(30,0,'3124 OPT'), (30,6,'3244 ERL'), (30,0,'2145 TAR'); 
+         -- voitures à 30 places : offre lite
+         -- voitures à 20 places : offre classic
+         -- voitures à 15 places : offre class
+         -- voitures à 12 places : offre premium
+         -- voitures à 9 places : offre VIP
+
+
+insert into Vehicules(nombre_places, places_dispo,matricule) values (20,3,'3215 TAV'), (20,3, '1234 MUW'),(20,1, '3412 TAR'),(30,0,'3124 OPT'), (30,6,'3244 ERL'), (30,0,'2145 TAR'),
+(30, 14, '3245 TAU'), (20, 14, '0943 POS'), (15, 9, '3425 WWP'), (15,2, '3249 TAP'), (15, 0, '3425 TRP'), (12, 6, '3255 ARP'), (12, 0,'2481 SQP'), (12, 4, '3021 PAS'),
+(9, 0, '3205 RAS'), (9,2, '3522 FZP'); 
