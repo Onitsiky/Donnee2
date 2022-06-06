a5191bcc76a9383f3f02c48d456f6ac9b7768990
Onitsiky <hei.onitsiky@gmail.com>
2022-06-06 21:49:18 +0300

    feat : adding new cities name

--- insert_Ville.sql
+++ insert_Ville.sql
@@ -1,1 +1,4 @@
 insert into Ville(nom_ville) values ('Antananarivo'),('Fianarantsoa'), ('Toamasina'),('Mahajanga'),('Morondava');
+
+insert into Ville(nom_ville) values ('Moramanga'),('Antsirabe'), ('Ambohimahasoa'), ('Brickaville'),
+('Miarinarivo'), ('Ambondromamy'), ('Maevatanana'), ('Ambositra'), ('Ankazobe');
