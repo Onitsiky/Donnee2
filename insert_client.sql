1d48b02941345e79a39784f9bbd0b4e7479e079d
njaina <98956159+njaina@users.noreply.github.com>
2022-06-08 13:11:36 +0200

    fix  : add more data in the "client" table

--- insert_client.sql
+++ insert_client.sql
@@ -1,8 +1,69 @@
-insert into Client(nom_client, prenom_client,contact) values ('RAKOTONANDRASANA', 'Jean Michel', '0345687298'),
-('RADRIANARIJAONA', 'Clément', '0328742798'), ('RAKOTOMANANA', 'Fitiavana','0323287409'),('RATSIRAHONANA', 'Henri', '0329883498'),
-('RAMANANARIVO','Ernest', '0334373287'),('RANDRIANALISOA', 'Emmanuel', '0324234254'),
-('FIHANTANA', 'Mitia', '0333287322'),('RAZANAMANANA', 'Marie', '0324364376'), ('RANDRIATSIFERANA', 'Ny Koloina', '0341234311'),
-('ZAFIMANANA', 'Mahafaly',  '0349853928'),('ANDRIAMANGESON', 'Frederic', '03487298539'),('FALY', 'Tantely', '0344534253'),
-('FALIMANANA', 'Jean Eric', '0325434309'), ('NIHANDOHANA', 'Fulgence', '0348598432'),('123', 'Society', '0202224335'), 
-('RAMS', 'Group', '0202234565'), ('RAJAONA', 'Fily', '0325465475'), ('JEAN JACK', 'Gilbert', '0345465434'), ('DUPRE', 'Lucile', '0345542154'),
-('TSIMIVADIKA', 'Laza', '0325365264');
+-- insert into client values 
+-- (1,'Ewbanke','Valérie','+261 340985933'),(2,'Gimert','Marylène','+261 387048358'),
+-- (3,'Peacey','Jean Michel','+261 329343953'),(4,'Andrée','Mariam','+261 349345302'),
+-- (5,'Legion','Sarobidy','+261 334285482'),(6,'Magrane Dù','Tán','+261 3843853923'),
+-- (7,'Comelini','Mercury','+261 384348299'),(8,'Farrey','Béatrice','+261 335472800'),
+-- (9,'Kitchingman','Salomé','+261 331249237'),(10,'Bouchier','Lauréna Kuí','+261 342449917');
+
+-- insert into client values 
+-- (11,'Woodburn','Börje','+261 348575701'),(12,'Sprowson','Maëly','+261 335472800'),
+-- (13,'Buttner','Nadège','+261 332386310'),(14,'Korlat','Cosgra','+261 326508145'),
+-- (15,'Donnell','Adèle','+261 324857886'),(16,'Åslög','Laïla','+261 345583566'),
+-- (17,'Addekin','Maïlys','+261 338798791'),(18,'Matteuzzi','Gérald','+261 331586298'),
+-- (19,'Dowdney','Véronique','+261 325998185'),(20,'Collington','Miléna','+261 349823012');
+
+insert into client values 
+(21,'Phateplace','McGrey','+261 331732868'),(22,'Tesseyman','Dawes','+261 382988834'),
+(23,'Fulloway','Vénus','+261 328443711'),(24,'Flobert','Erdely','+261 321658013'),
+(25,'Bérangère','Léandre','+261 347389294'),(26,'Petrelli','Régine','+261 321808593'),
+(27,'Utley','Mélia','+261 334591362'),(28,'Keyzor','Irène','+261 338045064'),
+(29,'Kù','Maïlis','+261 384168222'),(30,'Walles','Marlène','+261 343411820');
+
+insert into client values 
+(31,'Raden','Kuí','+261 321876652'),(32,'Jedrysik','Angèle','+261 349671751'),
+(33,'Vallens','Bérengère','+261 329939900'),(34,'Isakson','Åke','+261 331546640'),
+(35,'Faulo','Annotée','+261 337716213'),(36,'Munslow','Léa','+261 336856027'),
+(37,'Kirkup',''Mariethérèse','+261 321447702'),(38,'Witherdon','Eliès','+261 335865238'),
+(39,'McMoyer','Clémentine','+261 331769748'),(40,'Cockson','Jú','+261 326812447');
+
+insert into client values 
+(41,'Martugin','Mà','+261 325264510'),(42,'Yearnsley','Cécilia','+261 349446911'),
+(43,'Ainscow','Daphnée','+261 386988058'),(44,'Buckles','Edmée','+261 349564290'),
+(45,'Dunge','Léane','+261 340442161'),(46,'Yabsley','Méryl','+261 347606410'),
+(47,'Linnane','Bénédicte','+261 329469849'),(48,'Detoile','Táng','+261 337942781'),
+(49,'Lansley','Séréna','+261 347644568'),(50,'Fishby','Salomé','+261 325457679');
+
+insert into client values 
+(51,'Maudson','Marienoël','+261 331661709'),(52,'','',''),
+(53,'Clunan','Laurène','+261 342338832'),(54,'Shotter','Publicité','+261 335984176'),
+(55,'Bodicum','Méryl','+261 386703773'),(56,'Holstein','Maëline','+261 380373158'),
+(57,'Olivie','Magdalène','+261 327103519'),(58,'Heyball','Daphnée','+261 349366099'),
+(59,'Panchin','Nuó','+261 336511805'),(60,'McKeefry','Gaëlle','+261 381324429');
+
+insert into client values 
+(61,'Bedson','Maïly','+261 386677380'),(62,'Ainsley','Mélissandre','+261 326895155'),
+(63,'Brame','Anaëlle','+261 337753075'),(64,'Truss','Naéva','+261 323976868'),
+(65,'Murison','Stévina','+261 336383807'),(66,'Tierney','Lén','+261 347315304'),
+(67,'Schroter','Uò','+261 336539360'),(68,'Burndred','Edmée','+261 349666451'),
+(69,'Knowler','Marlène','+261 348610754'),(70,'Mayhead','Cléopatre','+261 335027245');
+
+insert into client values 
+(71,'Foyster','Mélys','+261 345944323'),(72,'','',''),
+(73,'Oslar','Cléopatre','+261 348165938'),(74,'Maudlen','Görel','+261 385467191'),
+(75,'Castro','Célia','+261 326853331'),(76,'Boddie','Lén','+261 322818414'),
+(77,'Chidley','Léonie','+261 385865300'),(78,'Craghead','Gösta','+261 341803032'),
+(79,'Landsman','Anaïs','+261 385916533'),(80,'ValensSmith','Anaël','+261 328030610');
+
+insert into client values 
+(81,'Stanesby','Léonore','+261 322284558'),(82,'Lynas','Naéva','+261 321686893'),
+(83,'Graeme','Måns','+261 320829865'),(84,'Coppins','Eliès','+261 326883073'),
+(85,'Dorre','Nuó','+261 325228432'),(86,'Hayto','Lauréna','+261 348860571'),
+(87,'McClory','Yáo','+261 328468472'),(88,'Baddeley','Maëlann','+261 330109558'),
+(89,'Slaughter','Léandre','+261 322642841'),(90,'Grumble','Mélys','+261 389244587'),
+
+insert into client values 
+(91,'Dyet','Andréanne','+261 329899369'),(92,'Lightwing','Yóu','+261 331117713'),
+(93,'Fendlen','Lài','+261 346623134'),(94,'Oleksinski','Mégane','+261 385445703'),
+(95,'McInnes','Lài','+261 331777898'),(96,'Mitford','Océanne','+261 327785213'),
+(97,'Rudolph','Bécassine','+261 332271491'),(98,'Scown','Dorothée','+261 321183837'),
+(99,'MacTrustie','Esbjörn','+261 343976530'),(100,'Chomley','Åslög','+261 337038492');
