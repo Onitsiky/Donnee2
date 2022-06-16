87db6d9d98eb907562e20feeff840f7d67487d06
njaina <98956159+njaina@users.noreply.github.com>
2022-06-16 18:58:50 +0300

    feat : deletion of the file outside the folders

--- Nom_clientNP.sql
+++
@@ -1,4 +0,0 @@
--- afficher les noms des clients qui n'ont pas encore paye leurs reservations
-SELECT nom_client, prenom_client FROM client 
-INNER JOIN reserver ON reserver.id_client = client.id_client 
-WHERE montant_paye = '0';
