43a5d7861c8195dad7ac86529179eb1679c6ad2c
njaina <98956159+njaina@users.noreply.github.com>
2022-06-16 19:02:45 +0300

    feat : deletion of the file outside the folders

--- client_NP.sql
+++
@@ -1,4 +0,0 @@
--- Combien de clients n'ont pas encore effectue leur payement en totalite? Lesquels?
-SELECT count(nom_client)  FROM client 
-INNER JOIN reserver ON reserver.id_client = client.id_client 
-WHERE montant_paye = '0';
