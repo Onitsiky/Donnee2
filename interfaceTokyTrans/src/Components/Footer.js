d98525420c60453aee204f1ec3922036deb3c4e6
Onitsiky <hei.onitsiky@gmail.com>
2022-06-23 22:40:06 +0300

    feat : update

---
+++ interfaceTokyTrans/src/Components/Footer.js
@@ -0,0 +1,24 @@
+export default function Footer(){
+    return(
+        <>
+            <div className="footer">
+                <div className="first">
+                    <h5>Contacts</h5>
+                    <br/>
+                    <br/>
+                    <p>Toky Transport</p> 
+                    <p>TokyTransport Ivandry, Antananarivo</p>
+                    <div className="footer-contain">
+                    <p>Phone : </p><p className="white"> +261 34 23 124 12</p> 
+                    </div>
+                    <div className="footer-contain">
+                    <p>E-mail : </p> <p className="white"> tokytransport@gmail.com</p> 
+                    </div>
+                    <div className="footer-contain">
+                    <p>Site Web : </p> <p className="white"> www.toky-transport.com</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
